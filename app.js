cef821f5635e70f7d70a6bc85028b136111ab2e8
@bobfalafel <shayshun3@gmail.com>
2023-04-26 21:50:04 +0300

    encryption and stuff

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const Joi = require('@hapi/joi');
 const app = express();
+const http = require('http').Server(app);
 const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
@@ -15,12 +17,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/cardproject')
     console.error('Error connecting to MongoDB:', err);
   });
 
-const contactSchema = Joi.object({
+const userValSchema = Joi.object({
   username: Joi.string().required().min(6).max(70),
   email: Joi.string().required().email(),
   password: Joi.string().required().min(8).max(18),
   submit: Joi.string()
 });
+
+const cardValSchema = Joi.object({
+    bname: Joi.string().min(2).max(255).required(),
+    desc: Joi.string().min(2).max(1024).required(),
+    address: Joi.string().min(2).max(400).required(),
+    phone: Joi.string().min(9).max(10).required().regex(/^0[2-9]\d{7,8}$/),
+    imgUrl: Joi.string().min(11).max(1024).required()
+  });
 
 const cardSchema = mongoose.Schema({
   bname: String,
@@ -29,24 +39,23 @@ const cardSchema = mongoose.Schema({
   phone: String,
   imgUrl: String,
   ownerId: Number,
-  id: Number
+  id: {type: Number, unique: true}
 });
 
 const userSchema = mongoose.Schema({
-  id: Number,
   username: String,
-  email: String,
+  email: {type: String, unique: true},
   password: String,
-  type: Boolean
+  business: Boolean
 });
 
 
 const Card = mongoose.model('Card', cardSchema);
 const User = mongoose.model('User', userSchema);
 
+app.use(express.json());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
@@ -64,7 +73,7 @@ app.get('/login', (req, res) => {
 
 
 app.post('/signup', async (req, res) => {
-  const { error, value } = contactSchema.validate(req.body);
+  const { error, value } = userValSchema.validate(req.body);
   if (error) {
     res.render('signup-fail', { title: 'Sign Up' });
   } else {
@@ -81,6 +90,8 @@ app.post('/signup', async (req, res) => {
           password: data.password,
           id: users.length + 1
         });
+        const salt = await bcrypt.genSalt();
+        user.password = await bcrypt.hash(user.password, salt);
         await user.save();
         res.redirect('/thanks');
       }
@@ -91,14 +102,27 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id, business: this.business }, 'tokenMaster');
+  return token;
+}
+
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email, password });
+    const user = await User.findOne({ email: req.body.email });
     if (user) {
-      res.redirect('/');
-      console.log("great success");
+      const validPass = await bcrypt.compare(req.body.password, user.password)
+      if(validPass) {
+        res.json({ token: user.generateAuthToken() });
+        res.redirect('/');
+        console.log("great success");
+      } else {
+        res.render('login-fail', { title: 'Sign Up' });;
+        console.log("bad");
+      }
     } else {
+      res.render('login-fail', { title: 'Sign Up' });;
       console.log("bad");
     }
   } catch (err) {
